6b3b3f56cd95aec2b1094e19412d1e14d071703c
NEKOxrx <albetpf085@gmail.com>
2024-06-23 12:58:52 +0800

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'The Flutter Way - Template',
+      title: 'Tugas UAS PBB',
       initialRoute: LoginScreen.routeName,
       routes: routes,
     );
